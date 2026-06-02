1e73438d25fd3309594bb91d9a1887220a689efe
krist0nian <krist0ian@yahoo.com>
2026-06-01 21:13:50 -0400

    Completed User Card

---
+++ src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios'
+
+const Home = () => {
+    const [users, setUsers] = useState([]);
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    async function getUsers(id) {
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+        setUsers(data);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    return (
+<div className="container">
+  <div className="row">
+    <div className="user-list">
+        {users.map((user) => (
+      <div className="user" key={user.id} onClick={() => navigate(`/${user.id}`)}>
+        <div className="user-card">
+          <div className="user-card__container">
+            <h3>{user.name}</h3>
+            <p>
+              <b>Email:</b> {user.email}
+            </p>
+            <p>
+              <b>Phone:</b> {user.phone}
+            </p>
+            <p>
+              <b>Website:</b>
+              {user.website}
+            </p>
+          </div>
+        </div>
+      </div>
+    ))}
+    </div>
+  </div>
+</div>
+    )
+}
+
+export default Home;
